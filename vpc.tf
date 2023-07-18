96bc211158e7ca486228deb1968b78be942c5e11
jhon-murillo <104637388+jhon-murillo@users.noreply.github.com>
2023-07-18 13:14:13 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,20 @@
+provider "aws" {
+  region = "us-east-1"  # Update with your desired AWS region
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.0"
+
+  # Configure the module inputs according to your requirements
+  name                 = "my-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["us-east-1a", "us-east-1b"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  # Configure other module inputs as necessary
 }
